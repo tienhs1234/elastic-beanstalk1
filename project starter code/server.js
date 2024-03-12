2ca1a46505414c078d35f79fb34b089b217456f6
TienTien <tienhs1234@gmail.com>
2024-03-12 09:40:45 +0700

    first commit

--- project starter code/server.js
+++ project starter code/server.js
@@ -13,6 +13,25 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+  const authenticationMiddleware = (req, res, next) => {
+    // Check for the presence of the Authorization header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN_VALUE
+
+    if (token == null) {
+        return res.sendStatus(401); // Unauthorized
+    }
+
+    // Validate the token
+    if (token !== "YOUR_ACCESS_TOKEN") {
+        return res.sendStatus(403); // Forbidden
+    }
+
+    next(); // Proceed to the next middleware/route handler
+};
+
+app.use(authenticationMiddleware)
+
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -35,6 +54,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
   // Displays a simple message to the user
   app.get( "/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
+  } );
+
+  app.get( "/filteredimage", async (req, res) => {
+    const filterImage = await filterImageFromURL(req.query.image_url)
+    res.set('File-URL', filterImage);
+    res.sendFile(filterImage);
+  } );
+
+  app.delete( "/delete", async (req, res) => {
+    await deleteLocalFiles([req.query.image_url])
   } );
   
 
